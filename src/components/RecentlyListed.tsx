ece9d4a2e8ff68cb64494036e4d4912abe9bede6
siddhant-k08 <kulkarni.sid0809@gmail.com>
2025-12-23 23:34:43 +0530

    Query added

--- src/components/RecentlyListed.tsx
+++ src/components/RecentlyListed.tsx
@@ -2,19 +2,56 @@ import { useQuery } from "@tanstack/react-query"
 import { useMemo } from "react"
 import NFTBox from "./NFTBox"
 import Link from "next/link"
-import { request } from "graphql/request"
-import { formatPrice } from "../utils/formatPrice"
 
-const GRAPHQL_ENDPOINT = process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT!
-
-interface ActiveListing {
-    nftAddresses: string
-    tokenId: string
-    price: string
-    seller: string
-    blockNumber: Number
+const GET_RECENT_NFTS = `
+query AllItemsListeds {
+ allItemListeds(first: 20, orderBy: [BLOCK_NUMBER_DESC, TX_INDEX_DESC]) {
+  nodes {
+    rindexerId
+    seller
+    nftAddress
+    price
+    tokenId
+    contractAddress
+    txHash
+    blockNumber
+  }
+ }
+ itemCanceled(nodeId: $nodeId) {
+  nftAddress
+  tokenId
+ }
+ itemBought(nodeId: $itemBoughtNodeId2) {
+  tokenId
+  nftAddress
+ }
 }
+`
 
-export default function RecentlyListedNFT() {
-    
+// Main component that uses the custom hook
+export default function RecentlyListedNFTs() {
+    return (
+        <div className="container mx-auto px-4 py-8">
+            <div className="mt-8 text-center">
+                <Link
+                    href="/list-nft"
+                    className="inline-block py-2 px-4 bg-blue-600 text-white rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+                >
+                    List Your NFT
+                </Link>
+            </div>
+            <h2 className="text-2xl font-bold mb-6">Recently Listed NFTs</h2>
+
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-6 gap-4">
+                <img
+                    src="/placeholder.png"
+                    alt={`NFT`}
+                    className="w-full h-auto max-h-96 object-contain bg-zinc-50"
+                    onError={() => {
+                        console.error("Error loading NFT image")
+                    }}
+                />
+            </div>
+        </div>
+    )
 }
